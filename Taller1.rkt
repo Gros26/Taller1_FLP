5a670a201d3ce8519c3bd164cfce5668e9e21b42
LisNat <rivera.liseth@correounivalle.edu.co>
2026-02-24 22:54:48 -0500

    Adelanto casos de prueba

--- Taller1.rkt
+++ Taller1.rkt
@@ -14,19 +14,22 @@
 
 ;------------------ 1 --------------------
 
-; <par> ::= (<int> <int>)
-; <lista-pares> ::= '()
-;               ::= (<par> <lista-pares>)
+;; invert :
+;; Proposito:
+;; Lista de pares x Predicado -> Lista de pares : Procedimiento que evalua una lista 
+;; de pares (x y). Si ambos elementos del par cumplen con el predicado P, invierte 
+;; el par a (y x) y lo conserva. Si alguno no cumple, el par es descartado
 
-;invert : lista-pares x Predicado -> lista-pares
-;usage: (invert L P) = retorna una lista con los pares invertidos (y x)solo
-;                      cuando ambos elementos del par cumplen el predicado P 
+;; <par> ::= (<int> <int>)
+;; <lista-pares> ::= ()
+;;               ::= (<par> <lista-pares>)
+
 (define invert
   (lambda (L P)
     (if (null? L)
         empty
         (if (and (P (car (car L))) (P (cadr (car L))))
-            (cons (crear-lista (cadr (car L)) (car (car L)))
+            (cons (list (cadr (car L)) (car (car L)))
                   (invert (cdr L) P))
             (invert (cdr L) P)
             )
@@ -34,14 +37,29 @@
     )
   )
 
+;; Pruebas
+; 1. Caso base
+(invert empty odd?) 
+
+; 2. Caso mixto: algunos pares cumplen y otros no
+(invert '((3 2) (4 2) (1 5) (2 8)) even?) 
+
+; 3. Caso donde todos los pares cumplen con el predicado
+(invert '((1 3) (5 7) (9 11)) odd?)
+
+; 4. Caso donde ningún par cumple con el predicado
+(invert '((2 4) (6 8)) odd?)
+
+
 ;------------------- 2 -----------------------
 
-; <lista> ::= '()
-;         ::= (<SchemeVal> <lista>)
+;; down :
+;; Proposito:
+;; Lista -> Lista : Procedimiento que retorna una nueva lista donde cada elemento 
+;; de la lista original L tiene un nivel adicional de parentesis
+;; <lista> ::= ()
+;;         ::= (<SchemeVal> <lista>)
 
-;down : lista -> lista
-;usage: (down L) = retorna una lista donde cada elemento de L tiene un
-;                  nivel más de parentésis
 (define down
   (lambda (L)
     (if (null? L)
@@ -52,11 +70,28 @@
     )
   )
 
+;; Pruebas
+; 1. Caso base
+(down empty)
+
+; 2. Caso con una lista plana de números
+(down '(1 2 3))
+; Resultado esperado: '((1) (2) (3))
+
+; 3. Caso con símbolos y listas anidadas
+(down '(a (hi (hello))))
+
+; 4. Caso con elementos que ya son listas
+(down '((a) (b)))
+
+
 ;----------------- 3 -------------------
 
-;list-set : lista x int x SchemeVal x predicado  -> lista
-;usage: (list-set L n x P) = retorna una lista similar a L, pero reemplaza el elemento en la
-;                            posición n por x, solo si el elemento original cumple con P.
+;; list-set :
+;; Proposito:
+;; Lista x Int x SchemeVal x Predicado -> Lista : Procedimiento que retorna una 
+;; lista similar a L, pero reemplaza el elemento en la posicion n (indexada desde 0) 
+;; por el valor x, solo si el elemento original en esa posicion satisface el predicado P
   
 (define list-set
   (lambda (L n x P)
@@ -72,11 +107,30 @@
     )
   )
 
+;; Pruebas
+; 1. Caso base
+(list-set '() 2 'x odd?)
+
+; 2. Caso en el que elemento en la posicion n cumple predicado
+(list-set '(1 2 4 5) 2 'x even?)
+
+; 3. Caso en el que el elemento en la posicion n no cumple el predicado
+(list-set '(1 3 4 5) 1 'x even?)
+
+; 4. Prueba indexación 0  
+(list-set '(a b c) 0 'z symbol?)
+
+; 5. Cado donde el indice n es mayor a la longitud de la lista
+(list-set '(1 2 3) 5 'x odd?)
+
+
 ;------------------ 4 ------------------------
 
-;filter-in : predicado x lista  -> lista
-;usage: (list-set L P) = retorna una lista que contiene los elementos de L
-;                            que satisfacen el predicado P
+;; filter-in :
+;; Proposito:
+;; Predicado x Lista -> Lista : Procedimiento que retorna una nueva lista que 
+;; contiene unicamente los elementos de L que satisfacen el predicado P
+
 (define filter-in
   (lambda (P L)
     (if (null? L)
@@ -89,14 +143,31 @@
     )
 )
 
+;; Pruebas
+; 1. Caso base
+(filter-in odd? empty)
+
+; 2. Caso con una lista mixta
+(filter-in symbol? '(a 1 b 2 c 3))
+
+; 3. Caso donde ningún elemento cumple el predicado
+(filter-in string? '(1 2 3 4))
+
+; 4. Caso donde filtramos solo los números pares de una lista de enteros
+(filter-in even? '(1 2 3 4 5 6))
+
+
 ;----------------- 5 --------------------
 
-; <lista-simbolos> ::= '()
-;                  ::= (<simbolo> <lista-simbolos>)
+;; reversa-aux :
+;; Proposito:
+;; Lista de Simbolos x Lista de Simbolos -> Lista de Simbolos : Procedimiento que 
+;; invierte la lista L moviendo sus elementos uno por uno al acumulador 'acc' 
+;; de forma recursiva
 
-; reversa-aux : lista x lista -> lista
-; usage: (reversa-aux L acc) = Invierte la lista L moviendo sus elementos uno por
-;                              uno al acumulador recursivamente.
+;; <lista-simbolos> ::= ()
+;;                  ::= (<Simbolo> <lista-simbolos>)
+
 (define reversa-aux
   (lambda (L acc)
     (if (null? L)
@@ -106,9 +177,11 @@
     )
   )
 
-;palindrome? : lista  -> bool
-;usage: (palindrome? palabra) = Retorna #t si la palabra se lee igual de izquierda
-;                               a derecha que de derecha a izquierda
+;; palindrome? :
+;; Proposito:
+;; Lista de Simbolos -> Bool : Procedimiento que determina si una lista de 
+;; simbolos es un palindromo, es decir, se lee exactamente igual de izquierda 
+;; a derecha que de derecha a izquierda
 
 (define palindrome?
   (lambda (palabra)
@@ -116,13 +189,30 @@
     )
   )
 
+;; Pruebas
+; 1. Caso base
+(palindrome? empty)
+
+; 2. Caso palindromo con longitud impar
+(palindrome? '(r a d a r))
+
+; 3. Caso palindromo con longitud par
+(palindrome? '(a n n a))
+
+; 4. Caso con palabra que no es palindroma
+(palindrome? '(m o t o))
+
+; 5. Palabra de un solo caracter 
+(palindrome? '(x))
+
 ;------------------ 6 --------------------
 
-; <lista> ::= '()
-;         ::= (<SchemeVal> <lista>)
-;swappper : SchemeVal x SchemeVal x lista -> lista
-;usage: (swapper E1 x E2 x L) = Retorna una lista similar a L pero cada
-;                               ocurrencia de E1 se reemplaza con E2, y viceversa.
+;; swapper :
+;; Proposito:
+;; SchemeVal x SchemeVal x Lista -> Lista : Procedimiento que retorna una lista 
+;; similar a L, pero donde cada ocurrencia del elemento E1 es reemplazada por E2, 
+;; y cada ocurrencia de E2 es reemplazada por E1
+
 (define swapper
   (lambda (E1 E2 L)
     (if (null? L)
@@ -138,11 +228,33 @@
     )
   )
 
+;; Pruebas
+; 1. Caso base
+(swapper 'x 'y empty)
+
+; 2. Caso donde se intercambian símbolos múltiples veces en una lista
+(swapper 'a 'b '(a b c d a b))
+
+; 3. Caso con lista de solo números
+(swapper 1 2 '(1 2 3 1 2))
+
+; 4. Caso donde los elementos a intercambiar no están en la lista
+(swapper 'x 'y '(a b c))
+
+; 5. Caso con lista mixta
+(swapper 7 'a '(c 7 3 a a))
+
+
 ;----------------- 7 ----------------------------------
 
-;tuple-maker : SchemeValue x lista -> lista
-;usage : (tuple-maker s L) = Retorna una lista de tuplas de todos los elementos
-;                            de L emparejados con el símbolo s
+;; tuple-maker :
+;; Proposito:
+;; SchemeVal x Lista -> Lista de Tuplas : Procedimiento que retorna una lista 
+;; de tuplas de todos los elementos de L emparejados con el símbolo s
+
+;; <lista-tuplas> ::= ()
+;;                ::= ((<SchemeVal> <SchemeVal>) <lista-tuplas>)
+
 (define tuple-maker
   (lambda (s L)
     (if (null? L)
@@ -154,9 +266,11 @@
     )
   )
 
-;append-list : lista x lista -> lista
-;usage : (append L1 L2) = Concatena las listas retornando una nueva lista con
-;                        todos los elementos de L1 seguido de los elementos de L2.
+;; append-list :
+;; Proposito:
+;; Lista x Lista -> Lista : Procedimiento que concatena dos listas, retornando 
+;; una nueva lista con todos los elementos de L1 seguidos de los elementos de L2
+
 (define append-list
   (lambda (L1 L2)
     (if (null? L1)
@@ -165,9 +279,12 @@
         )
     )
   )
-;cartesian-product : lista-simbolos x lista-simbolos -> lista
-;usage : (cartesian-product L1 L2) = Retorna la lista de tuplas de simbolos
-;                                    resultado del producto cartesiano de ambas listas.
+
+;; cartesian-product :
+;; Proposito:
+;; Lista de Simbolos x Lista de Simbolos -> Lista de Tuplas : Procedimiento que retorna
+;; una lista de tuplas correspondientes al producto cartesiano entre ambas listas
+
 (define cartesian-product
   (lambda (L1 L2)
     (if (null? L1)
@@ -177,11 +294,31 @@
     )
   )
 
+;; Pruebas
+; 1. Caso donde L1 es vacia
+(cartesian-product '() '(x y))
+
+; 2. Caso donde L2 es vacia
+(cartesian-product '(1 4 7) '())
+
+; 3. Caso de dos listas normales 
+(cartesian-product '(a b c) '(x y))
+
+; 4. Producto cartesiano con una lista diferentes (enteros y simbolos)
+(cartesian-product '(p q) '(1 2 3))
+
+; 5. Caso donde ambas listas tienen un solo elemento
+(cartesian-product '(z) '(w))
+
+
 ;----------------------- 8 ------------------
 
-;mapping : funcion x lista x lista -> lista
-;usage : (mapping F L1 L2) = Retorna una lista de tuplas (a_n b_n) dónde a pertenece la posición n en L1
-;                            b pertenece a la posición n en L2 y se cumple que F(a) = b
+;; mapping :
+;; Proposito:
+;; Funcion x Lista x Lista -> Lista de Tuplas : Procedimiento que retorna una 
+;; lista de tuplas (a_n b_n), dónde a pertenece la posición n en L1, b
+;; pertenece a la posición n en L2 y se cumple que F(a) = b
+
 (define mapping
   (lambda (F L1 L2)
     (if (null? L1)
@@ -195,11 +332,27 @@
     )
   )
 
+;; Pruebas
+; 1. Caso base
+(mapping (lambda (x) x) empty empty)
+
+; 2. Caso en donde todos cumplen
+(mapping (lambda (d) (* d 2)) '(1 2 3) '(2 4 6))
+
+; 3. Caso donde solo algunos elementos cumplen la condición
+(mapping (lambda (d) (* d 2)) '(1 2 3) '(3 4 5))
+
+; 4. Usando predicados de Racket
+(mapping even? '(2 3 4) '(#t #f #t))
+
 
 ;------------------- 9 -----------------------------
 
-; count-smaller : int x lista -> Int
-; usage: (count-smaller x L) = retorna la cantidad de elementos en L que son menores que x.
+;; count-smaller : (Funcion Auxiliar)
+;; Proposito:
+;; Int x Lista de Enteros -> Int : Procedimiento que retorna la cantidad de elementos 
+;; en la lista L que son estrictamente menores que el numero x
+
 (define count-smaller
   (lambda (x L)
     (if (null? L)
@@ -209,8 +362,12 @@
         )
     )
   )
-; inversions : lista -> int
-; usage: (inversions L) = retorna el número de inversiones en L
+
+;; inversions :
+;; Proposito:
+;; Lista de Enteros -> Int : Procedimiento que retorna el numero total de inversiones 
+;; en una lista L.
+
 (define inversions
   (lambda (L)
     (if (null? L)
@@ -221,15 +378,30 @@
     )
   )
 
-;--------------------- 10 --------------------
+;; Pruebas para inversions (y por extension, count-smaller)
+; 1. Caso base
+(inversions empty)
 
-; <lista-parentesis> ::= '()
-;         ::= (<parentesis> <lista-parebtesus>)
-;<parentesis> ::= ( | )
-;
-; balanced-parentheses? : lista -> Bool
-; usage: (alanced-parentheses? L) retorna #t si los paréntesis estan balanceados, 
-;                                 de lo contrario retorna #f
+; 2. Caso donde no hay inversiones posibles (orden descendente)
+(inversions '(1 2 3 4 5))
+
+; 3. Caso con el número máximo de inversiones (orden ascendente)
+(inversions '(5 4 3 2 1))
+
+; 4. Caso con lista desordenada
+(inversions '(2 3 8 6 1))
+
+
+;--------------------- 10 --------------------
+;; balanced-parentheses? :
+;; Proposito:
+;; List -> Bool : Procedimiento que determina si los paréntesis estan balanceados donde
+;;  O representa a ( y C representa a )
+;;
+;; <lista-parentesis> ::= ()
+;;                    ::= (<parentesis> <lista-parentesis>)
+;; <parentesis>       ::= O | C
+
 (define balanced-parentheses?
   (lambda (L)
     (letrec 
@@ -241,8 +413,8 @@
                 (if (null? lista)
                     (zero? cont) 
                     (cond
-                      [(equal? (car lista) '"(") (balance-aux (cdr lista) (+ cont 1))]
-                      [(equal? (car lista) '")") (balance-aux (cdr lista) (- cont 1))]
+                      [(equal? (car lista) 'O) (balance-aux (cdr lista) (+ cont 1))]
+                      [(equal? (car lista) 'C) (balance-aux (cdr lista) (- cont 1))]
                       [else (balance-aux (cdr lista) cont)]
                       )
                     )
@@ -255,19 +427,29 @@
     )
   )
 
+;; Pruebas
+; 1. Caso base
+(balanced-parentheses? empty)
 
-; para hacer las pruebas deben colocar ""
-; con el ejemplo del profe sería algo así:
-; Prueba 1: ()() -> Debería ser #t
-;(balanced-parentheses? '("(" ")" "(" ")"))
-;(balanced-parentheses? '("(" "(" ")" ")" "(" ")"))
+; 2. Caso con paréntesis balanceados anidados
+(balanced-parentheses? '(O O C C))
 
+; 3. Caso con paréntesis balanceados secuenciales
+(balanced-parentheses? '(O C O C))
+
+; 4. Caso de desbalance por exceso de cierres 
+(balanced-parentheses? '(O O C C C))
+
+; 5. Caso de desbalance por empezar cerrando
+(balanced-parentheses? '(C O C O))
 
 ;---------------- 11 --------------------
 
-;zip : Function x List x List -> List
-;usage : (zip f l1 l2) = Una lista donde la posicion n-esima corresponde al resultado de aplicar
-;                        la funcion F sobre los elementos en la posicion n-esima en L1 y L2.
+;; zip :
+;; Proposito:
+;; Funcion x Lista x Lista -> Lista : Procedimiento que retorna una nueva lista 
+;; donde la posicion n-esima corresponde al resultado de aplicar la funcion F 
+;;  sobre los elementos en la posicion n-esima en L1 y L2
 
 (define zip
   (lambda (f l1 l2)
@@ -278,11 +460,27 @@
     )
   )
 
+;; Pruebas
+; 1. Caso base
+(zip * empty empty)
+
+; 2. Caso con operador matematico
+(zip + '(1 4) '(6 2))
+
+; 3. Caso con operador logico para comparar elementos posicion a posicion
+(zip equal? '(a b c) '(a x c))
+
+; 4. Caso usando list para agrupar elementos de ambas listas en sublistas
+(zip list '(a b c) '(1 2 3))
+
 
 ;-------------- 12 ----------------------
 
-;filter-acum : Int x Int x Function x Int x Function
-;usage : (filter-acum a b f acum filter) = El resultado acum de aplicar la funcion f a los elementos en [a,b] que cumplen filter
+;; filter-acum :
+;; Proposito:
+;; Int x Int x Funcion x Int x Funcion -> Int : Procedimiento que aplica la funcion f
+;; a un valor acumulado "acum" y retorna el resultado "acum" de aplicar la funcion f 
+;; a los elementos en [a,b] que cumplen filter
 
 (define filter-acum
   (lambda (a b f acum filter)
@@ -298,6 +496,16 @@
 
 
 (filter-acum 1 10 + 0 odd?)
+
+;; Pruebas
+; 1. Caso de suma (numeros impares entre 1 y 10)
+(filter-acum 1 10 + 0 odd?)
+
+; 2. Caso de multiplicación (numeros pares entre 1 y 5)
+(filter-acum 1 5 * 1 even?)
+
+; 3. Caso con rango es invalido
+(filter-acum 5 1 + 0 even?)
 
 
 ;------------------ 13 --------------
@@ -322,16 +530,20 @@
     )
   )
 
+
 ;---------------- 14 ---------------
 
-; <arbol-binario> ::= (arbol-vacio) empty
-;                 ::= (nodo) numero <arbol-binario> <arbol-binario>)
-; path : n x BST -> lista
-; usage: (path n BST) = retorna una lista con la ruta desde la raíz hasta n 
+;; path :
+;; Proposito:
+;; Int x BST -> Lista de Simbolos : Procedimiento retorna una lista de simbolos 
+;; ('left y 'right) que representa la ruta desde la raiz hasta el nodo que contiene a 'n'.
+
+;; <arbol-binario> ::= empty
+;;                 ::= (<Int> <arbol-binario> <arbol-binario>)
 
 (define path
   (lambda (n BST)
-    (if (null? BST)    ; n esta garantizado, lo dejamos o quitamos?
+    (if (null? BST)    
         empty
         (cond
           [(= n (car BST)) empty]
@@ -340,6 +552,20 @@
         )
     )
   )
+
+;; Pruebas
+; 1. Buscar la raiz
+(path 8 '(8 (3 (1 empty empty) (6 empty empty)) (10 empty (14 empty empty))))
+
+; 2. Buscar un nodo que esta a la izquierda y luego a la derecha
+(path 6 '(8 (3 (1 empty empty) (6 empty empty)) (10 empty (14 empty empty))))
+
+; 3. Buscar un nodo profundo a la derecha
+(path 14 '(8 (3 (1 empty empty) (6 empty empty)) (10 empty (14 empty empty))))
+
+; 4. Buscar el nodo mas a la izquierda
+(path 1 '(8 (3 (1 empty empty) (6 empty empty)) (10 empty (14 empty empty))))
+
 
 ;---------------- 15 ---------------
 
